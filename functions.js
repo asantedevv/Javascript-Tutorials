9a96e2a92abe89187f8d8657e3c1681f784bc155
Kwame Asante Junior <vie97pie@gmail.com>
2024-06-02 17:16:16 +0000

    functions to change paragraph content

---
+++ functions.js
@@ -0,0 +1,7 @@
+function changeParagraph() {
+    document.getElementById("demo").innerHTML = "Paragraph changed"
+}
+
+function reverseParagraph() {
+    document.getElementById("demo").innerHTML = "A paragraph"
+}
